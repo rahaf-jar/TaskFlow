e791f3a7b3d76bdfa219f650caf3861c95073847
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2026-07-24 22:15:07 +0200

    Created Employees table structure

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -9,3 +9,16 @@ CREATE TABLE Departments (
     department_name VARCHAR(100) NOT NULL,
     manager_name VARCHAR(100)
 );
+
+
+CREATE TABLE Employees (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    job_title VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES Departments(department_id)
+);
