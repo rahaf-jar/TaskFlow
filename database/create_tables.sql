b0df19f096d3458cd2ab6141aa862af25fa92014
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2026-07-24 22:00:32 +0200

    Created Departments table structure

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -3,3 +3,9 @@ DROP DATABASE IF EXISTS taskflow;
 CREATE DATABASE taskflow;
 
 USE taskflow;
+
+CREATE TABLE Departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100) NOT NULL,
+    manager_name VARCHAR(100)
+);
